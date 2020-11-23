c21c5d876e396c53a6129135b5769ed146747baf
Will Lane <williamlane923@gmail.com>
2020-11-23 08:44:25 -0800

    made it so natls wouldnt try to get file times on files with creation dates or modified dates that dont exist

--- src/utils/file_times.rs
+++ src/utils/file_times.rs
@@ -2,26 +2,34 @@ use chrono;
 use filetime;
 
 pub fn modified(file: std::path::PathBuf, format: String) -> String {
-  let naive = chrono::NaiveDateTime::from_timestamp(
-    filetime::FileTime::from_last_modification_time(&file.symlink_metadata().unwrap()).seconds()
+  if file.symlink_metadata().unwrap().modified().is_ok() {
+    let naive = chrono::NaiveDateTime::from_timestamp(
+      filetime::FileTime::from_last_modification_time(&file.symlink_metadata().unwrap()).seconds()
       as i64,
-    0,
-  );
+      0,
+      );
 
-  let datetime: chrono::DateTime<chrono::Local> =
-    chrono::DateTime::from_utc(naive, *chrono::Local::now().offset());
-  datetime.format(&format).to_string()
+    let datetime: chrono::DateTime<chrono::Local> =
+      chrono::DateTime::from_utc(naive, *chrono::Local::now().offset());
+    datetime.format(&format).to_string()
+  } else {
+    return "00 000 00:00:00".to_string()
+  }
 }
 
 pub fn created(file: std::path::PathBuf, format: String) -> String {
-  let naive = chrono::NaiveDateTime::from_timestamp(
-    filetime::FileTime::from_creation_time(&file.symlink_metadata().unwrap())
+  if file.symlink_metadata().unwrap().created().is_ok() {
+    let naive = chrono::NaiveDateTime::from_timestamp(
+      filetime::FileTime::from_creation_time(&file.symlink_metadata().unwrap())
       .unwrap()
       .seconds() as i64,
-    0,
-  );
+      0,
+      );
 
-  let datetime: chrono::DateTime<chrono::Local> =
-    chrono::DateTime::from_utc(naive, *chrono::Local::now().offset());
-  datetime.format(&format).to_string()
+    let datetime: chrono::DateTime<chrono::Local> =
+      chrono::DateTime::from_utc(naive, *chrono::Local::now().offset());
+    datetime.format(&format).to_string()
+  } else {
+    return "00 000 00:00:00".to_string()
+  }
 }
