f8e49de78528c8048a069b223de42647ec2ec9c4
Denys Vitali <denys@denv.it>
2021-03-14 22:41:41 +0100

    feat: change default time format (HH:MM)

--- src/input.rs
+++ src/input.rs
@@ -34,7 +34,7 @@ pub struct Cli {
   pub long: bool,
 
   /// Formats the time output 
-  #[structopt(long = "time-format", default_value = "%e %b %T")]
+  #[structopt(long = "time-format", default_value = "%e %b %H.%M")]
   pub time_format: String,
 
   /// Shows the file created time instead of the file modified time
